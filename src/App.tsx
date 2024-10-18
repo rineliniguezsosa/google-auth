06197c4f5646220d6a2a9aa78d0c6b24fd162b9b
Rinel <rineliniguezsosa@gmail.com>
2024-10-17 23:09:51 -0600

    [FEAT] Multiples cambios en el archivo App.tsx
    - importando useEffect
    - creando la variable   googleClientid para obtener el la variable de entorno
    - creando el useEffect para crear la variable sdkgoogle donde creamos el elemento del script, al elemento sdkgoogle le establecemos la propp src con la url de la carga del sdk de google, al sdkgoogle le establecemos las props async y defer en true para poder establecerles estas props
    - retornamos el clean up para eliminar el script del dom del método useEffect
    - cambie el fragmento que envolvia el retorno de la app por un div donde implemente css para establecer display flex y poder ver el botón
    - estableci el contenido del botón de google donde estableci el valor de la googleClientid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  const googleClientid = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+  useEffect(()=>{
+    const sdkgoogle = document.createElement('script');
+    sdkgoogle.src ="https://accounts.google.com/gsi/client";
+    sdkgoogle.async = true;
+    sdkgoogle.defer = true;
+
+    document.body.appendChild(sdkgoogle);
+
+    return () => {
+      document.body.removeChild(sdkgoogle);  // Limpiar el script cuando el componente se desmonte
+    };
+  },[]);
+
   return (
-    <>
+    <div style={{display:'flex',justifyContent:'center',border:'1px red solid',width:'100%',height:'500px'}}>
       <h1>Google auth</h1>
-    </>
+
+      <div id="g_id_onload"
+          data-client_id={googleClientid}
+          data-auto_prompt="false"
+          data-callback="handleCredentialResponse"
+        >
+      </div>
+      <div className="g_id_signin"
+          data-type="standard"
+          data-size="large"
+          data-theme="outline"
+          data-text="sign_in_with"
+          data-shape="rectangular"
+          data-logo_alignment="left">
+      </div>
+    </div>
   )
 }
 
